1b5533350b7605aa62aa58e7b683ad333e5f47e7
Jonathan <jm5967a@student.american.edu>
2017-05-05 18:35:38 -0400

    Delete Repeat

--- clojure-quirk/clojure-quirk/test/clojure_quirk/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clojure-quirk.core-test
-  (:require [clojure.test :refer :all]
-            [clojure-quirk.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
